7f0d8999d4bfb046dcd70589568d640ca2ddb5b6
JoeyMucci <jmucci314@gmail.com>
2024-02-22 14:20:05 -0500

    Created star for rating system

---
+++ web/src/components/Star/Star.jsx
@@ -0,0 +1,11 @@
+import starOutline from '../../../public/starOutline.png'
+
+const Star = () => {
+  return (
+    <div>
+      <img src={starOutline} alt="Star" />
+    </div>
+  )
+}
+
+export default Star
