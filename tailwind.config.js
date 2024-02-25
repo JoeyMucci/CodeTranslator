3f6eb4ebbdf07fb00b24b91c546cf18b4fa612e1
root <root@DESKTOP-7V7B53V>
2024-02-25 18:17:24 -0500

    configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  //purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- web/src/pages/CodeTranslatorPage/CodeTranslatorPage.jsx
+++ web/src/pages/CodeTranslatorPage/CodeTranslatorPage.jsx
@@ -4,17 +4,23 @@ import { Metadata } from '@redwoodjs/web'
 const CodeTranslatorPage = () => {
   return (
     <>
-      <Metadata title="CodeTranslator" description="CodeTranslator page" />
+      <div className="bg-blue-300 min-h-screen">
+        <Metadata title="CodeTranslator" description="CodeTranslator page" />
 
-      <h1>CodeTranslatorPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/CodeTranslatorPage/CodeTranslatorPage.jsx</code>
-      </p>
-      <p>
-        My default route is named <code>codeTranslator</code>, link to me with `
-        <Link to={routes.codeTranslator()}>CodeTranslator</Link>`
-      </p>
+        <h1 className="text-3xl underline">CodeTranslatorPage</h1>
+        <h2 className="text-3xl underline font-bold bg-blue-500"> s </h2>
+        <p>
+          Find me in{' '}
+          <code>./web/src/pages/CodeTranslatorPage/CodeTranslatorPage.jsx</code>
+        </p>
+        <p>
+          My default route is named <code>codeTranslator</code>, link to me with
+          `<Link to={routes.codeTranslator()}>CodeTranslator</Link>`
+        </p>
+        <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+          {'Test'}
+        </button>
+      </div>
     </>
   )
 }
