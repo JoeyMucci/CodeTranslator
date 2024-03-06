3c523884530ad5d447a19df272ba318a9072d24f
haresh38 <98345441+haresh38@users.noreply.github.com>
2024-03-05 20:01:49 -0500

    Create LoginPage.test.jsx

---
+++ web/src/pages/LoginPage/LoginPage.test.jsx
@@ -0,0 +1,12 @@
+import { render } from '@redwoodjs/testing/web'
+
+import LoginPage from './LoginPage'
+
+describe('LoginPage', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<LoginPage />)
+    }).not.toThrow()
+  })
+
+})
