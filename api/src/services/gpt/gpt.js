deadab1a86932b1965762a703a738bc946faf808
JoeyMucci <jmucci314@gmail.com>
2024-03-08 23:54:33 -0500

    added optimization, dont know how promises work

--- api/src/services/gpt/gpt.js
+++ api/src/services/gpt/gpt.js
@@ -3,18 +3,37 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const OpenAI = require('openai')
 
 const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY, // This is also the default, can be omitted
+  apiKey: OPENAI_API_KEY,
 })
 
-export const runTranslation = async ({ fromLanguage, toLanguage, code }) => {
-  const chatCompletion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'user',
-        content: 'Translate the following ' + { fromLanguage } + ' code to ' + { toLanguage } + ' code: ' + { code },
-      },
-    ],
-  })
-  return chatCompletion.choices[0].message.content
+async function runTranslation({ fromLanguage, toLanguage, code }) {
+  if (fromLanguage == toLanguage) {
+    const chatCompletion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: 'Optimize the following ' + fromLanguage + ' code: ' + code,
+        },
+      ],
+    })
+    return chatCompletion.choices[0].message.content
+  } else {
+    const chatCompletion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: 'Translate the following ' + fromLanguage + ' code to ' + toLanguage + ' code: ' + code,
+        },
+      ],
+    })
+    return chatCompletion.choices[0].message.content
+  }
 }
+const input = { fromLanguage: 'Python', toLanguage: 'C', code: 'print("hello world")' }
+const inputtwo = { fromLanguage: 'Python', toLanguage: 'Python', code: 'x=0\nx+=1\nx+=1\nprint(x)' }
+const result = runTranslation(input).resolve()
+const resulttwo = runTranslation(inputtwo).resolve()
+console.log(result)
+console.log(resulttwo)
