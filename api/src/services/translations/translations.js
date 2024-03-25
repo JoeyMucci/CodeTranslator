cce25428ef6d0713d98091b14d949cf37955dae7
haresh38 <98345441+haresh38@users.noreply.github.com>
2024-03-24 22:22:32 -0400

    Update translations.js with resolvers

--- api/src/services/translations/translations.js
+++ api/src/services/translations/translations.js
@@ -1,10 +1,25 @@
+
 import { db } from 'src/lib/db'
+
+export const translations = () => {
+ return db.translation.findMany()
+}
 
 export const createTranslation = ({ input }) => {
  return db.translation.create({
-    data: {
-      ...input,
-      status: 'pending',
-    },
+    data: input,
+ })
+}
+
+export const updateTranslation = ({ id, input }) => {
+ return db.translation.update({
+    where: { id },
+    data: input,
+ })
+}
+
+export const deleteTranslation = ({ id }) => {
+ return db.translation.delete({
+    where: { id },
  })
 }
