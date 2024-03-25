cce3bea6d9f582b49e1d2fb9837a24dc673f44fd
haresh38 <98345441+haresh38@users.noreply.github.com>
2024-03-24 21:42:09 -0400

    Create translations.js

---
+++ api/src/services/translations/translations.js
@@ -0,0 +1,10 @@
+import { db } from 'src/lib/db'
+
+export const createTranslation = ({ input }) => {
+ return db.translation.create({
+    data: {
+      ...input,
+      status: 'pending',
+    },
+ })
+}
