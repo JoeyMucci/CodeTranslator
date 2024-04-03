663bbb39fbd386e4403a0621b2576f6c39e70a02
root <root@DESKTOP-7V7B53V>
2024-04-02 22:07:15 -0400

    added download and copy buttons to translations along with general reformatting/touch ups

--- web/src/components/FAQ.jsx
+++ web/src/components/FAQ.jsx
@@ -2,27 +2,12 @@ import React from 'react'
 
 function FAQ({ faq, index, toggleFAQ }) {
   return (
-<<<<<<< HEAD
-    <div className={"hover:bg-blue-200 faq " + (faq.open ? 'open' : '')}
-         key={index}
-         onClick={() => toggleFAQ(index)}
-    >
-      <div className="faq-question  ">
-        {faq.question}
-      </div>
-
-      <div className="faq-answer">
-        {faq.answer}
-      </div>
-
-=======
     <div
       className={'faq hover:bg-blue-200 ' + (faq.open ? 'open' : '')}
       key={index}
       onClick={() => toggleFAQ(index)}
     >
       <div className="faq-question">{faq.question}</div>
->>>>>>> 520577d1d4bdf3411910d0bd0d4176326a197331
 
       <div className="faq-answer">{faq.answer}</div>
     </div>
--- web/src/pages/TranslationHistoryPage/TranslationHistoryPage.jsx
+++ web/src/pages/TranslationHistoryPage/TranslationHistoryPage.jsx
@@ -3,6 +3,8 @@ import RecordsCell from 'web/src/components/RecordsCell/RecordsCell.jsx'
 
 import { Metadata } from '@redwoodjs/web'
 
+import arrow from 'web/public/Arrow.png';
+
 const RecordhistoryPage = () => {
   let emil = localStorage.getItem('userEmail')
   return (
@@ -13,6 +15,56 @@ const RecordhistoryPage = () => {
       />
 
       <h1 className="text">Translation History</h1>
+  <div className="flex flex-row justify-center space-x-20">
+    <div className="OriginalFilter basis-1/4" style={{width: '600px'}}>
+      <label htmlFor="language" className="text-white">
+              Filter by Input Language:
+            </label>
+            <div className="flex flex-row justify-between">
+              <select
+                name="language"
+                id="language"
+                className="mt-1 h-7 w-20 basis-3/4 rounded bg-text_box text-center hover:bg-blue-200 "
+              >
+                <option value="Default">All Languages</option>
+                <option value="C">C</option>
+                <option value="C++">C++</option>
+                <option value="Java">Java</option>
+                <option value="PHP">PHP</option>
+                <option value="Python">Python</option>
+                <option value="SQL">SQL</option>
+              </select>
+            </div>
+    </div>
+    <div className="flex flex-col justify-center items-center ">
+          <img
+            alt="arrow"
+            className="basis-1/2;"
+            src={arrow}
+            style={{ width: '75px', height: '50px', align: 'middle', opacity: '0'}}
+          ></img>
+        </div>
+    <div className="OriginalFilter basis-1/4" style={{width: '600px'}}>
+      <label htmlFor="language" className="text-white">
+      Filter by Output Language:
+            </label>
+            <div className="flex flex-row justify-between">
+              <select
+                name="language"
+                id="language"
+                className="mt-1 h-7 w-20 basis-3/4 rounded bg-text_box text-center hover:bg-blue-200 "
+              >
+                <option value="Default">All Languages</option>
+                <option value="C">C</option>
+                <option value="C++">C++</option>
+                <option value="Java">Java</option>
+                <option value="PHP">PHP</option>
+                <option value="Python">Python</option>
+                <option value="SQL">SQL</option>
+              </select>
+            </div>
+    </div>
+  </div>
       <RecordsCell goal={emil} />
     </>
   )
