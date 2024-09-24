0b6672673a9d3bb86cd3ce6b7f839e8808000448
JoeyMucci <31972810+JoeyMucci@users.noreply.github.com>
2024-09-24 11:33:17 -0400

    Update forgot.js
    changed email to match the api key on netlify

--- api/src/services/forgot/forgot.js
+++ api/src/services/forgot/forgot.js
@@ -32,7 +32,7 @@ export const sendPasswordResetEmail = async (to, resetLink) => {
   var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail()
 
   sendSmtpEmail.subject = 'Rosetta Code Password Reset'
-  sendSmtpEmail.sender = { name: 'Rosetta Code', email: 'RosettaCodeOfficial@gmail.com' }
+  sendSmtpEmail.sender = { name: 'Rosetta Code', email: 'jmucci314@gmail.com' }
   sendSmtpEmail.type = 'classic'
   sendSmtpEmail.htmlContent = `${resetLink}` // Assuming text is HTML content
   sendSmtpEmail.to = [{ email: `${to}` }]
